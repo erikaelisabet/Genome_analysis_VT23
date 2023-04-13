e508bd2f21033c82f76e1cdbdfb9d37df4f9c678
Erika Lindberg <erikali@rackham3.uppmax.uu.se>
2023-04-13 13:14:43 +0200

    working assembly script canu

--- code/01_ass_DNA.sh
+++ code/01_ass_DNA.sh
@@ -1,25 +1,22 @@
-
 #!/bin/bash -l
 #SBATCH -A uppmax2023-2-8
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 16:00:00
 #SBATCH -J canu_job
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Erika.Lindberg.3300@student.uu.se
+
 # Load modules
 module load bioinfo-tools
 module load canu 
 
 # Your commands
-export outdir = /home/erikali/Genome_analysis_VT23/analyses/01_DNA_ass
-canu -num_threads 2 \
--d outdir \
--p ass_L_Ferriphilum \
-useGrid=false \
-genomeSize=2.41 \
--pacbio-raw /home/erikali/Genome_analysis_VT23/data/raw_data/DNA_raw_data/*.fastq.gz
+OUTPUTPATH="/home/erikali/Genome_analysis_VT23/analyses/01_DNA_ass"
+INPUT="/proj/genomeanalysis2023/Genome_Analysis/2_Christel_2017/DNA_raw_data/ERR2028*.fastq.gz"
+
+canu -d $OUTPUTPATH -p LFerriphilum genomeSize=2.6m useGrid=false maxThreads=4 -raw -pacbio $INPUT
 
 
 
