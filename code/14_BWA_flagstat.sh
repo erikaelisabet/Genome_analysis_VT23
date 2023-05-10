166ba5b10f5f6f768f5a9a38b3c721e2accf2e07
Erika Lindberg <erikali@rackham3.uppmax.uu.se>
2023-05-10 16:22:11 +0200

    stats BWA

---
+++ code/14_BWA_flagstat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2023-2-8
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 16:00:00
+#SBATCH -J BWA_stats
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Erika.Lindberg.3300@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load bwa
+module load samtools
+
+# variables
+OUTPUT="/home/erikali/Genome_analysis_VT23/analyses/04_RNA_mapping"
+INPUT="/proj/genomeanalysis2023/nobackup/work/erikalindberg"
+SEQUENCES="ERR2036629 ERR2036630 ERR2036631 ERR2036632 ERR2036633 ERR2117288 ERR2117289 ERR2117290 ERR2117291 ERR2117292"
+
+$SNIC_TMP
+
+cd $SNIC_TMP
+
+for F in $SEQUENCES
+do
+(
+gunzip -c $INPUT/${F}_mapped_RNA.sorted.bam.gz > $SNIC_TMP/${F}_mapped_RNA.sorted.bam
+samtools flagstat -O tsv $SNIC_TMP/${F}_mapped_RNA.sorted.bam > $SNIC_TMP/${F}.flagstat
+) &
+done
+wait 
+
+cp $SNIC_TMP/*.flagstat $OUTPUT
